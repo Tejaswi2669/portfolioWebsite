aa49819ede7089585966678734529ac7eafc3ebc
Tejaswi2669 <tejaswinerella9@gmail.com>
2024-10-18 14:47:32 +0200

    Added basic components and updated the App componets and created Routes to the other components.

--- portfolio-website/src/app/app.module.ts
+++ portfolio-website/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+   import { BrowserModule } from '@angular/platform-browser';
+   import { RouterModule, Routes } from '@angular/router';
+   import { AppComponent } from './app.component';
+   import { AboutMeComponent } from './about-me/about-me.component';
+   import { ProjectsComponent } from './projects/projects.component';
+   import { CertificatesComponent } from './certificates/certificates.component';
+   import { EducationComponent } from './education/education.component';
 
-import { AppComponent } from './app.component';
+   const routes: Routes = [
+     { path: '', redirectTo: '/about', pathMatch: 'full' },
+     { path: 'about', component: AboutMeComponent },
+     { path: 'projects', component: ProjectsComponent },
+     { path: 'certificates', component: CertificatesComponent },
+     { path: 'education', component: EducationComponent }
+   ];
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+   @NgModule({
+     declarations: [
+       AppComponent,
+       AboutMeComponent,
+       ProjectsComponent,
+       CertificatesComponent,
+       EducationComponent
+     ],
+     imports: [
+       BrowserModule,
+       RouterModule.forRoot(routes)
+     ],
+     providers: [],
+     bootstrap: [AppComponent]
+   })
+   export class AppModule { }
+   
