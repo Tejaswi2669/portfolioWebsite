466737061a4e46e0c44148343a6a9d0c7c58636b
Tejaswi2669 <tejaswinerella9@gmail.com>
2024-10-19 16:41:34 +0200

    Added translation package and translated about me section

--- portfolio-website/src/app/app.module.ts
+++ portfolio-website/src/app/app.module.ts
@@ -1,34 +1,50 @@
 import { NgModule } from '@angular/core';
-   import { BrowserModule } from '@angular/platform-browser';
-   import { RouterModule, Routes } from '@angular/router';
-   import { AppComponent } from './app.component';
-   import { AboutMeComponent } from './about-me/about-me.component';
-   import { ProjectsComponent } from './projects/projects.component';
-   import { CertificatesComponent } from './certificates/certificates.component';
-   import { EducationComponent } from './education/education.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-   const routes: Routes = [
-     { path: '', redirectTo: '/about', pathMatch: 'full' },
-     { path: 'about', component: AboutMeComponent },
-     { path: 'projects', component: ProjectsComponent },
-     { path: 'certificates', component: CertificatesComponent },
-     { path: 'education', component: EducationComponent }
-   ];
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { EducationComponent } from './education/education.component';
+import { RouterModule, Routes } from '@angular/router';
 
-   @NgModule({
-     declarations: [
-       AppComponent,
-       AboutMeComponent,
-       ProjectsComponent,
-       CertificatesComponent,
-       EducationComponent
-     ],
-     imports: [
-       BrowserModule,
-       RouterModule.forRoot(routes)
-     ],
-     providers: [],
-     bootstrap: [AppComponent]
-   })
-   export class AppModule { }
-   
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'certificates', component: CertificatesComponent },
+  { path: 'education', component: EducationComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    CertificatesComponent,
+    EducationComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
