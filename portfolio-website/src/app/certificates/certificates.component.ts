54cbde532a0ec5ca4c0790856fb6dbf76297976c
Tejaswi2669 <tejaswinerella9@gmail.com>
2024-10-23 23:09:49 +0200

    Added certificates and its translation, changed css for about me component

--- portfolio-website/src/app/certificates/certificates.component.ts
+++ portfolio-website/src/app/certificates/certificates.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-certificates',
   templateUrl: './certificates.component.html',
   styleUrls: ['./certificates.component.css']
 })
-export class CertificatesComponent implements OnInit {
+export class CertificatesComponent {
+  certificates: { title: string; description: string; link: string; }[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe(() => {
+      this.loadCertificates();
+    });
+    this.translate.get('certificates').subscribe(() => {
+      this.loadCertificates();
+    });
   }
 
+  loadCertificates() {
+    this.certificates = [
+      {
+        title: this.translate.instant('certificates.angular.title'),
+        description: this.translate.instant('certificates.angular.description'),
+        link: 'https://www.linkedin.com/learning/certificates/5ea82ab4ca38c445a4a72242f3f841d51cd74921cd0688895f68b151e52ef1b6?trk=share_certificate'
+      },
+      {
+        title: this.translate.instant('certificates.powerBI.title'),
+        description: this.translate.instant('certificates.powerBI.description'),
+        link: 'https://www.linkedin.com/learning/certificates/d571ce221ff6e07fd8dc642428b4600376117019c89188374b633aa2edfb7ae6?trk=share_certificate'
+      },
+      {
+        title: this.translate.instant('certificates.sqlAdvanced.title'),
+        description: this.translate.instant('certificates.sqlAdvanced.description'),
+        link: 'https://www.hackerrank.com/certificates/e550d72f7283'
+      },
+      {
+        title: this.translate.instant('certificates.sqlIntermediate.title'),
+        description: this.translate.instant('certificates.sqlIntermediate.description'),
+        link: 'https://www.hackerrank.com/certificates/35d2e7f0700b'
+      }
+    ];
+  }
 }
--- portfolio-website/src/app/projects/projects.component.ts
+++ portfolio-website/src/app/projects/projects.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProjectsComponent {
+  projects: { title: string; description: string; technologies: string; }[] = [];
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe(() => {
+      this.loadProjects();
+    });
+    this.translate.get('certificates').subscribe(() => {
+      this.loadProjects();
+    });
   }
-
+  loadProjects() {
+  this.projects = [
+    {
+      title: this.translate.instant('bloodDonationSystem.title'),
+      description: this.translate.instant('bloodDonationSystem.description'),
+      technologies: this.translate.instant('bloodDonationSystem.technologies'),
+    },
+    {
+      title: this.translate.instant('movieChatbot.title'),
+      description: this.translate.instant('movieChatbot.description'),
+      technologies: this.translate.instant('movieChatbot.technologies'),
+    },
+    {
+      title: this.translate.instant('astrologyApp.title'),
+      description: this.translate.instant('astrologyApp.description'),
+      technologies: this.translate.instant('astrologyApp.technologies'),
+    },
+    {
+      title: this.translate.instant('portfolioWebsite.title'),
+      description: this.translate.instant('portfolioWebsite.description'),
+      technologies: this.translate.instant('portfolioWebsite.technologies'),
+    }
+  ];
+  }
+  
 }
